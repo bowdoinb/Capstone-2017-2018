2f6327f47da70ffb5a16aa7ee13f3551c92885b7
jshellab <jshellab@indiana.edu>
2018-02-22 16:25:28 -0500

    ViewGroups complete

--- app/FindMe/app/src/main/java/com/example/blakebowdoin/findme/ViewGroupActivity.java
+++ app/FindMe/app/src/main/java/com/example/blakebowdoin/findme/ViewGroupActivity.java
@@ -5,27 +5,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class ViewGroupActivity extends AppCompatActivity {
 
     ListView listView;
+    String username;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_group);
 
         listView = (ListView) findViewById(R.id.listView);
-        getJSON("http://cgi.soic.indiana.edu/~jshellab/ViewGroups.php");
+        getJSON("http://cgi.soic.indiana.edu/~team48/FindMeViewGroups.php");
+
+        username = getIntent().getExtras().getString("username");
     }
 
     private void getJSON(final String urlWebService){
@@ -33,14 +39,9 @@ public class ViewGroupActivity extends AppCompatActivity {
         class GetJSON extends AsyncTask<Void, Void, String>{
 
             @Override
-            protected void onPreExecute(){
-                super.onPreExecute();
-            }
-
-            @Override
             protected void onPostExecute(String s){
-                super.onPostExecute(s);
-                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+                //super.onPostExecute(s);
+                //Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
                 try{
                     loadIntoListView(s);
                 }catch (JSONException e){
@@ -54,6 +55,16 @@ public class ViewGroupActivity extends AppCompatActivity {
                 try{
                     URL url = new URL(urlWebService);
                     HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setDoOutput(true);
+                    con.setDoInput(true);
+                    OutputStream outputStream = con.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("username", "UTF-8")+"="+URLEncoder.encode(username, "UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
                     StringBuilder sb = new StringBuilder();
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
                     String json;
